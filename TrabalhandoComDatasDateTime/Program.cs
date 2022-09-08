c3559b0012b6a127e913d8a13f42118829a2e7dd
bmotadev <bmotadev@gmail.com>
2022-09-08 15:25:52 -0300

    utilizando ToShortDateString e ToLongDateString

--- TrabalhandoComDatasDateTime/Program.cs
+++ TrabalhandoComDatasDateTime/Program.cs
@@ -14,7 +14,8 @@ namespace TrabalhandoComDatasDateTime
             var sixMonthsLater = today.AddMonths(6);
             var twoYearsLater = today.AddYears(2);
 
-            
+            var shortDate = now.ToShortDateString();
+            var longDate = now.ToLongDateString();
 
             Console.ReadKey();
         }
