dcfd5cd605931ce8e6dac5c4ddcb43eed856fcbc
bmotadev <bmotadev@gmail.com>
2022-09-30 14:40:13 -0300

    trabalhando com diretorios

---
+++ TrabalhandoComDiretorios/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace TrabalhandoComDiretorios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Directory e DirectoryInfo
+            var folderName = "pasta";
+            var subFolderName = "subpasta";
+            var subFolderNameStatic = "subpasta_usingstatic";
+
+            var directoryInfoSubFolder = new DirectoryInfo(subFolderName);
+
+            if (!Directory.Exists(subFolderName) || !directoryInfoSubFolder.Exists)
+            {
+                Directory.CreateDirectory(subFolderNameStatic);
+                Directory.CreateDirectory(folderName);
+                directoryInfoSubFolder.Create();
+
+                directoryInfoSubFolder.MoveTo($"{folderName}//{subFolderName}"); // forma 1
+                Directory.Move(subFolderNameStatic, @$"{folderName}\{subFolderNameStatic}"); //forma 2
+            }
+
+            var name = directoryInfoSubFolder.Name;
+            var parent = directoryInfoSubFolder.Parent;
+            var root = directoryInfoSubFolder.Root;
+            var exists = directoryInfoSubFolder.Exists;
+
+            foreach (var directory in Directory.GetDirectories(folderName))
+            {
+                Console.WriteLine(directory);
+            }
+
+            Directory.Delete($@"{folderName}\{subFolderName}");
+
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
