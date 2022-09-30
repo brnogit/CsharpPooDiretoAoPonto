08d15c130f3ba13723d819ab1b26902983fcf221
bmotadev <bmotadev@gmail.com>
2022-09-30 15:07:53 -0300

    trabalhando com arquivos

---
+++ TrabalhandoComArquivos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace TrabalhandoComArquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region File e FileInfo
+            var file = @"pasta\texto.txt";
+
+            if (!File.Exists(file))
+            {
+                File.CreateText(file);
+            }
+
+            var fileInfo = new FileInfo(file);
+
+            Console.WriteLine($"Nome: {fileInfo.Name}, Tamanho: {fileInfo.Length}, Data de atualização: {fileInfo.LastWriteTime}");
+
+
+
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
