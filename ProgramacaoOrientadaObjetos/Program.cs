fd44b0fef9825b63470be99644b7fc25121f5d51
bmotadev <bmotadev@gmail.com>
2022-10-01 17:32:53 -0300

    conceitos do polimorfismo

--- ProgramacaoOrientadaObjetos/Program.cs
+++ ProgramacaoOrientadaObjetos/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ProgramacaoOrientadaObjetos
 {
@@ -6,7 +7,17 @@ namespace ProgramacaoOrientadaObjetos
     {
         static void Main(string[] args)
         {
-            
+            var professor = new Professor(5000m, "João", "12345", new DateTime(1980, 1, 1));
+            var estudante = new Estudante("A", "Luis", "554321", new DateTime(1992, 1, 1));
+
+            var pessoas = new List<Pessoa> { estudante, professor };
+
+            foreach (var pessoa in pessoas)
+            {
+                pessoa.SeApresentar();
+            }
+
+            Console.Read();
         }
     }
 }
