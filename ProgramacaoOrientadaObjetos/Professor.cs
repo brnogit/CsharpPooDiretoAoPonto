65a8c464dfc6193c4f10f521ca2d813e426b82f4
bmotadev <bmotadev@gmail.com>
2022-10-01 17:28:28 -0300

    adicionados propriedades a classe Professor

--- ProgramacaoOrientadaObjetos/Professor.cs
+++ ProgramacaoOrientadaObjetos/Professor.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace ProgramacaoOrientadaObjetos
 {
-    public class Professor
+    public class Professor : Pessoa
     {
+        public Professor(decimal salario, string nome, string documento, DateTime dataNascimento) : base(nome, documento, dataNascimento)
+        {
+            Salario = salario;
+            Turmas = new List<string> { "A", "B" };
+        }
+        public List<string> Turmas { get; private set; }
+        public decimal Salario { get; private set; }
     }
 }
